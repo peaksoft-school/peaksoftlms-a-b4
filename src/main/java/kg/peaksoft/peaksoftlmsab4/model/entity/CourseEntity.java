d4f456c12b289006dd2a7f09732cf2dcc28705e7
Talgarbek00 <talgarbeknazarbekov00@gmail.com>
2022-04-20 13:01:54 +0600

    test 2

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/CourseEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/CourseEntity.java
@@ -1,10 +1,7 @@
 package kg.peaksoft.peaksoftlmsab4.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -12,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
 @Table(name = "courses")
 @AllArgsConstructor
 @NoArgsConstructor
