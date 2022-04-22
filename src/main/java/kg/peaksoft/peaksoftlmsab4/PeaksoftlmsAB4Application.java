a69ee5fbc3dd21e2c486fa78ea667d95213af1e2
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-04-22 10:28:38 +0600

    Test deploy v2

--- src/main/java/kg/peaksoft/peaksoftlmsab4/PeaksoftlmsAB4Application.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/PeaksoftlmsAB4Application.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -19,6 +20,11 @@ public class PeaksoftlmsAB4Application {
 
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/")
+    public String greetingPage() {
+        return "<h1>Welcome to Peaksoftlms-A application!!!<h1/>";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PeaksoftlmsAB4Application.class, args);
