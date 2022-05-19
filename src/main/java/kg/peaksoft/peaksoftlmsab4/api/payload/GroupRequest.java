891bbf4116f2c076a8ed63aa4dd3d161a909bfc0
Talgarbek00 <talgarbeknazarbekov00@gmail.com>
2022-05-19 18:40:55 +0600

    update course pagination and update localDate

--- src/main/java/kg/peaksoft/peaksoftlmsab4/api/payload/GroupRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/api/payload/GroupRequest.java
@@ -15,7 +15,7 @@ public class GroupRequest {
     private String groupName;
 
     @NotBlank
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateOfStart;
 
     @NotBlank
