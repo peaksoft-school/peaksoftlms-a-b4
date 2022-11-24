c157c698fe8743e3f96b6991b6ede02d21fa241a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-24 17:25:20 +0600

    refactor ApplicationConfig

--- src/main/java/kg/peaksoft/peaksoftlmsab4/config/ApplicationConfig.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/config/ApplicationConfig.java
@@ -4,12 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public ModelMapper getModelMapper(){
+    public ModelMapper getModelMapper() {
         return new ModelMapper();
     }
+
 }
