5feb4ceb2e636c8ed0a3e67968e3b2a4106328b5
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-24 17:30:08 +0600

    refactor AuthApi

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/AuthApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/AuthApi.java
@@ -4,23 +4,25 @@ import io.swagger.v3.oas.annotations.Operation;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.AuthRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.AuthResponse;
 import kg.peaksoft.peaksoftlmsab4.service.serviceImpl.AuthServiceImpl;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.security.PermitAll;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/authentication")
-@AllArgsConstructor
-@CrossOrigin(origins = "*",maxAge = 3600)
+@RequestMapping("api/public")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AuthApi {
 
     private final AuthServiceImpl authService;
 
+    @Operation(summary = "Login", description = "Any user can authenticate")
     @PostMapping
-    @PermitAll
-    @Operation(summary = "authenticate", description = "it is authenticate http method")
-    public AuthResponse authenticate(@RequestBody AuthRequest authRequest) {
-        return authService.authenticate(authRequest);
+    public AuthResponse authenticate(@RequestBody AuthRequest request) {
+        return authService.authenticate(request);
     }
+
 }
