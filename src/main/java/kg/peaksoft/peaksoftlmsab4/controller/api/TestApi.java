a64ea848eebf59144725a7993a2d04ee51d152fd
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-26 00:40:46 +0600

    refactor TestApi v3

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/TestApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/TestApi.java
@@ -1,5 +1,6 @@
 package kg.peaksoft.peaksoftlmsab4.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.TestRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.TestResponse;
@@ -28,26 +29,31 @@ public class TestApi {
 
     private final TestService service;
 
+    @Operation(summary = "Create test", description = "Create test by lesson id")
     @PostMapping("{lessonId}")
-    public TestResponse create(@RequestBody TestRequest testRequest, @PathVariable Long lessonId) {
-        return service.create(testRequest, lessonId);
+    public TestResponse create(@RequestBody TestRequest request, @PathVariable Long lessonId) {
+        return service.create(request, lessonId);
     }
 
+    @Operation(summary = "Update test", description = "Update test")
     @PutMapping("{id}")
-    public TestResponse update(@PathVariable Long id, @RequestBody TestRequest testRequest) {
-        return service.update(id, testRequest);
+    public TestResponse update(@PathVariable Long id, @RequestBody TestRequest request) {
+        return service.update(id, request);
     }
 
+    @Operation(summary = "Get test", description = "Get test by id")
     @GetMapping("{id}")
     public TestResponse finById(@PathVariable Long id) {
         return service.findById(id);
     }
 
+    @Operation(summary = "Delete test", description = "Delete test by id")
     @DeleteMapping("{id}")
     public TestResponse deleteById(@PathVariable Long id) {
         return service.deleteById(id);
     }
 
+    @Operation(summary = "Get all tests", description = "Get all tests")
     @GetMapping
     public List<TestResponse> findAll() {
         return service.findAll();
