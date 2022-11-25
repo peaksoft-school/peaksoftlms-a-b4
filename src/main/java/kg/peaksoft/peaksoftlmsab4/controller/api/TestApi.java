8020ac349933281766e5e6b318775b797f52b2b0
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-25 23:45:07 +0600

    refactor TestApi

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/TestApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/TestApi.java
@@ -1,43 +1,55 @@
 package kg.peaksoft.peaksoftlmsab4.controller.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.TestRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.TestResponse;
 import kg.peaksoft.peaksoftlmsab4.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/tests")
+@RequestMapping("api/tests")
 @PreAuthorize("hasAuthority('INSTRUCTOR')")
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Test API", description = "Test endpoints")
 public class TestApi {
 
     private final TestService service;
 
     @PostMapping("/save/{lessonId}")
-    public TestResponse create(@RequestBody TestRequest testRequest,@PathVariable Long lessonId){
-        return service.create(testRequest,lessonId);
+    public TestResponse create(@RequestBody TestRequest testRequest, @PathVariable Long lessonId) {
+        return service.create(testRequest, lessonId);
     }
 
     @PutMapping("/update/{id}")
-    public TestResponse update(@PathVariable Long id,@RequestBody TestRequest testRequest){
+    public TestResponse update(@PathVariable Long id, @RequestBody TestRequest testRequest) {
         return service.update(id, testRequest);
     }
+
     @GetMapping("/get/{id}")
-    public TestResponse finById(@PathVariable Long id){
+    public TestResponse finById(@PathVariable Long id) {
         return service.findById(id);
     }
 
     @DeleteMapping("/delete/{id}")
-    public TestResponse deleteById(@PathVariable Long id){
+    public TestResponse deleteById(@PathVariable Long id) {
         return service.deleteById(id);
     }
+
     @GetMapping("/all")
-    public List<TestResponse>findAll(){
+    public List<TestResponse> findAll() {
         return service.findAll();
     }
 
