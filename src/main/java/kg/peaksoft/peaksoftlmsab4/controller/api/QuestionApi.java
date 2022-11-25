413f12f81c120528e0757a8f63ff10dbdd5f8f16
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-25 23:30:24 +0600

    refactor QuestionApi v3

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/QuestionApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/QuestionApi.java
@@ -1,5 +1,6 @@
 package kg.peaksoft.peaksoftlmsab4.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.QuestionRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.QuestionResponse;
@@ -28,31 +29,37 @@ public class QuestionApi {
 
     private final QuestionService service;
 
+    @Operation(summary = "Create question", description = "Save a new question")
     @PostMapping
-    public QuestionResponse create(@PathVariable Long id, @RequestBody QuestionRequest questionRequest) {
-        return service.create(id, questionRequest);
+    public QuestionResponse create(@PathVariable Long id, @RequestBody QuestionRequest request) {
+        return service.create(id, request);
     }
 
+    @Operation(summary = "Update question", description = "Update a existing question")
     @PutMapping("{id}")
-    public QuestionResponse update(@PathVariable Long id, @RequestBody QuestionRequest questionRequest) {
-        return service.update(id, questionRequest);
+    public QuestionResponse update(@PathVariable Long id, @RequestBody QuestionRequest request) {
+        return service.update(id, request);
     }
 
+    @Operation(summary = "Set question to test", description = "Setting question to test")
     @PutMapping("{testId}/set/{questionId}")
     public QuestionResponse findByQuestionToTest(@PathVariable Long testId, @PathVariable Long questionId) {
         return service.findByQuestionToTest(testId, questionId);
     }
 
+    @Operation(summary = "Get question", description = "Get a existing question by id")
     @GetMapping("{id}")
     public QuestionResponse findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
+    @Operation(summary = "Delete question", description = "Delete a existing question by id")
     @DeleteMapping("{id}")
     public QuestionResponse deleteById(@PathVariable Long id) {
         return service.deleteById(id);
     }
 
+    @Operation(summary = "Get all questions", description = "Get all existing questions")
     @GetMapping
     public List<QuestionResponse> findAll() {
         return service.findAll();
