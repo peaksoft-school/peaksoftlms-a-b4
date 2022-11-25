3fead595ce5d9cf03a64de570355cefc762ce8fa
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-25 23:41:46 +0600

    refactor TaskApi

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/TaskApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/TaskApi.java
@@ -5,18 +5,26 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.TaskRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.TaskResponse;
 import kg.peaksoft.peaksoftlmsab4.service.TaskService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/tasks")
 @PreAuthorize("hasAuthority('INSTRUCTOR')")
-@CrossOrigin(origins = "*",maxAge = 3600)
-@Tag(name = "Task", description = "The Task CRUD operations")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Task API", description = "Task endpoints")
 public class TaskApi {
 
     private final TaskService taskService;
@@ -24,8 +32,8 @@ public class TaskApi {
     @Operation(summary = "Creates new entity: Task",
             description = "This method saves new task. Only instructors can add new task to lesson")
     @PostMapping("/{lessonId}")
-    public TaskResponse saveTask(@RequestBody TaskRequest taskRequest,@PathVariable Long lessonId){
-        return taskService.saveTask(lessonId,taskRequest);
+    public TaskResponse saveTask(@RequestBody TaskRequest taskRequest, @PathVariable Long lessonId) {
+        return taskService.saveTask(lessonId, taskRequest);
     }
 
     @Operation(summary = "Gets a single tasks by identifier",
