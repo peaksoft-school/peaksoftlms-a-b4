2f07bfd42fff5b1be3fc9788d012c6cfb7f899f9
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-25 23:21:15 +0600

    refactor QuestionApi

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/QuestionApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/QuestionApi.java
@@ -1,35 +1,46 @@
 package kg.peaksoft.peaksoftlmsab4.controller.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.QuestionRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.QuestionResponse;
 import kg.peaksoft.peaksoftlmsab4.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/question")
+@RequestMapping("api/questions")
 @PreAuthorize("hasAuthority('INSTRUCTOR')")
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Question API", description = "Question endpoints")
 public class QuestionApi {
 
     private final QuestionService service;
 
     @PostMapping("/save")
-    public QuestionResponse create(@PathVariable Long id,@RequestBody QuestionRequest questionRequest) {
-        return service.create(id,questionRequest);
+    public QuestionResponse create(@PathVariable Long id, @RequestBody QuestionRequest questionRequest) {
+        return service.create(id, questionRequest);
     }
 
     @PutMapping("/update/{id}")
-    public QuestionResponse update(@PathVariable Long id,@RequestBody QuestionRequest questionRequest) {
+    public QuestionResponse update(@PathVariable Long id, @RequestBody QuestionRequest questionRequest) {
         return service.update(id, questionRequest);
     }
+
     @PutMapping("/{testId}/set/{questionId}")
-    public QuestionResponse findByQuestionToTest(@PathVariable Long testId,@PathVariable Long questionId){
-        return service.findByQuestionToTest(testId,questionId);
+    public QuestionResponse findByQuestionToTest(@PathVariable Long testId, @PathVariable Long questionId) {
+        return service.findByQuestionToTest(testId, questionId);
     }
 
     @GetMapping("/get/{id}")
