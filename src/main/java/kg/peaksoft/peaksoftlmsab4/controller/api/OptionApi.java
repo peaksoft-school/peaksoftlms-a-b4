e00670697236bf140af3d570e5c4214e5c66d1fa
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-25 23:04:03 +0600

    refactor OptionApi

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/OptionApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/api/OptionApi.java
@@ -5,22 +5,32 @@ import kg.peaksoft.peaksoftlmsab4.controller.payload.OptionResponse;
 import kg.peaksoft.peaksoftlmsab4.service.OptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/options")
+@RequestMapping("api/options")
 @PreAuthorize("hasAuthority('INSTRUCTOR')")
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class OptionApi {
     private final OptionService service;
 
     @PostMapping("/save")
     public OptionResponse create(@RequestBody OptionRequest optionRequest) {
         return service.create(optionRequest);
-    }    @PutMapping("/update/{id}")
+    }
+
+    @PutMapping("/update/{id}")
     public OptionResponse update(@PathVariable Long id, @RequestBody OptionRequest optionRequest) {
         return service.update(id, optionRequest);
     }
