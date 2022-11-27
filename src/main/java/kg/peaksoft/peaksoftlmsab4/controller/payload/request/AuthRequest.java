4d3b956a6b3b2a396686f5229427f934c08e489a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-27 15:46:31 +0600

    refactor AuthRequest

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/request/AuthRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/request/AuthRequest.java
@@ -8,8 +8,11 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class AuthRequest {
+
     @NotBlank
     private String email;
+
     @NotBlank
     private String password;
+
 }
