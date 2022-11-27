68dab1dce27e0e9de08bab9a8510ca9136b8e628
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-27 15:54:37 +0600

    refactor TestRequest

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/request/TestRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/request/TestRequest.java
@@ -1,16 +1,16 @@
 package kg.peaksoft.peaksoftlmsab4.controller.payload.request;
 
-import kg.peaksoft.peaksoftlmsab4.controller.payload.request.QuestionRequest;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-
 @Getter
 @Setter
 public class TestRequest {
+
     private String testName;
     private List<QuestionRequest> questions;
     private Long lessonId;
+
 }
