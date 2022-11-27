f20eb5b6772e01b039b6c4fc76889989f405df9d
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-27 15:48:24 +0600

    refactor InstructorRequest

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/request/InstructorRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/request/InstructorRequest.java
@@ -8,16 +8,22 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class InstructorRequest {
+
     @NotBlank
     private String firstName;
+
     @NotBlank
     private String lastName;
+
     @NotBlank
     private String phoneNumber;
+
     @NotBlank
     private String specialization;
+
     @NotBlank
     private String password;
+
     @NotBlank
     private String email;
 
