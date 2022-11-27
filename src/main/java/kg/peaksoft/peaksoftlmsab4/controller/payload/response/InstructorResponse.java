628ff955d99d4d15866d205df8bbd1a99f4a6f32
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-27 15:58:42 +0600

    refactor InstructorResponse

--- src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/response/InstructorResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/controller/payload/response/InstructorResponse.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class InstructorResponse {
+
     private String id;
     private String fullName;
     private String phoneNumber;
     private String specialization;
     private String email;
+
 }
