3e9743d0903dbdb067b633d1bd260f4e3a15e3a7
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 15:07:09 +0600

    refactor OptionEntity

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/OptionEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/OptionEntity.java
@@ -1,27 +1,30 @@
 package kg.peaksoft.peaksoftlmsab4.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
-@Table(name = "options")
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "options")
 public class OptionEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String option;
-    private Boolean isTrue;
 
+    private String option;
+
+    private Boolean isTrue;
 
 }
