1cbefd34fdb575a37b0ff0805cd663aa7cfd050e
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 17:28:49 +0600

    refactor TestStudentRepository

--- src/main/java/kg/peaksoft/peaksoftlmsab4/repository/TestStudentRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/repository/TestStudentRepository.java
@@ -11,7 +11,4 @@ public interface TestStudentRepository extends JpaRepository<TestStudentEntity, 
     @Query("select s from TestStudentEntity s where s.studentEntity.authInfo.email=?1")
     TestStudentEntity getByEmail(String email);
 
-    @Query("select case when count(a)>0 then true else false end" +
-            " from TestStudentEntity a where a.studentEntity.authInfo.email =?1")
-    boolean existsByEmail(String email);
 }
