09bbf2a9f06f4eafbc383a4a58cb3b6fbfe19a76
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 17:24:59 +0600

    refactor PresentationRepository

--- src/main/java/kg/peaksoft/peaksoftlmsab4/repository/PresentationRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/repository/PresentationRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PresentationRepository extends JpaRepository<PresentationEntity,Long> {
+public interface PresentationRepository extends JpaRepository<PresentationEntity, Long> {
+
 }
