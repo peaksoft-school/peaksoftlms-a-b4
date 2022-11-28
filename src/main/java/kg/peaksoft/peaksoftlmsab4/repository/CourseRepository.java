24150ec456e19d7e088b3cd792cf64cfd303451c
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 17:21:16 +0600

    refactor CourseRepository

--- src/main/java/kg/peaksoft/peaksoftlmsab4/repository/CourseRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/repository/CourseRepository.java
@@ -5,14 +5,13 @@ import kg.peaksoft.peaksoftlmsab4.model.entity.StudentEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<CourseEntity, Long> {
+
     boolean existsByCourseName(String courseName);
 
     @Query("select c from CourseEntity c")
@@ -20,4 +19,5 @@ public interface CourseRepository extends JpaRepository<CourseEntity, Long> {
 
     @Query("select s from StudentEntity s inner join s.courses c where c.id = :id order by s.id asc ")
     List<StudentEntity> getStudents(Long id);
+
 }
