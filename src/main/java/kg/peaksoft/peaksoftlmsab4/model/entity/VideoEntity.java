72e2c47a1f7d89c5da7f71d15e689147066166cf
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 15:47:41 +0600

    refactor VideoEntity v2

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/VideoEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/VideoEntity.java
@@ -31,23 +31,20 @@ import static javax.persistence.CascadeType.REFRESH;
 public class VideoEntity {
 
     @Id
-    @SequenceGenerator(
-            name = "videos_sequence",
-            sequenceName = "videos_id_seq",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "videos_sequence"
-    )
+    @SequenceGenerator(name = "videos_gen", sequenceName = "videos_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "videos_gen")
     private Long id;
+
     @Column(name = "video_name")
     private String videoName;
+
     private String description;
+
     @Column(name = "video_link")
     private String videoLink;
 
     @OneToOne(cascade = {MERGE, DETACH, REFRESH}, fetch = FetchType.LAZY)
     @JoinColumn(name = "lesson_id")
     private LessonEntity lessonEntity;
+
 }
