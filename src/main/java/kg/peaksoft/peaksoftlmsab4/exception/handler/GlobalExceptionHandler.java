e450bad45b810dd8c907e48f1bc45e12527cb69f
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 14:11:54 +0600

    refactor GlobalExceptionHandler

--- src/main/java/kg/peaksoft/peaksoftlmsab4/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/exception/handler/GlobalExceptionHandler.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -59,4 +62,5 @@ public class GlobalExceptionHandler {
                 .message(invalidArgumentException.getMessage())
                 .build();
     }
+
 }
