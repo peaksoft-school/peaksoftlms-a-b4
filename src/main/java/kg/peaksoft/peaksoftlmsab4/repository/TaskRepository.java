abf045749f8134197cf043b6898b42c7a8599c72
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 17:27:37 +0600

    refactor TaskRepository

--- src/main/java/kg/peaksoft/peaksoftlmsab4/repository/TaskRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/repository/TaskRepository.java
@@ -3,5 +3,6 @@ package kg.peaksoft.peaksoftlmsab4.repository;
 import kg.peaksoft.peaksoftlmsab4.model.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<TaskEntity,Long> {
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
 }
