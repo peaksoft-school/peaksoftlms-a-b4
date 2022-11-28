d3fecb6d0b29ad41948904e86d1f47b888556d4f
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 14:13:07 +0600

    refactor InvalidArgumentException

--- src/main/java/kg/peaksoft/peaksoftlmsab4/exception/InvalidArgumentException.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/exception/InvalidArgumentException.java
@@ -1,11 +1,9 @@
 package kg.peaksoft.peaksoftlmsab4.exception;
 
 public class InvalidArgumentException extends RuntimeException {
-    public InvalidArgumentException() {
-        super();
-    }
 
     public InvalidArgumentException(String message) {
         super(message);
     }
+
 }
