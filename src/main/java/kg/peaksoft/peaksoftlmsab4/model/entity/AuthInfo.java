1531c65e05a1b94dc516522eae9a0741ddb3fd08
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 14:30:07 +0600

    refactor AuthInfo

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/AuthInfo.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/AuthInfo.java
@@ -8,27 +8,43 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.util.Collection;
 import java.util.Collections;
 
-@Entity
-@Table(name = "auth_info")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "auth_info")
 public class AuthInfo implements UserDetails {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "auth_info__gen", sequenceName = "auth_info_seq", allocationSize = 1, initialValue = 2)
+    @GeneratedValue(generator = "auth_info__gen", strategy = GenerationType.SEQUENCE)
     Long id;
+
     String email;
+
     String password;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
     private boolean isAccountNonExpired = true;
+
     private boolean isAccountNonLocked = true;
+
     private boolean isCredentialsNonExpired = true;
+
     private boolean isEnabled = true;
 
     @Override
