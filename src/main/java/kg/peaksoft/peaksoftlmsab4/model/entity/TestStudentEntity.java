2cea1188335a7566d15394f1f2aa73260009d993
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 15:43:00 +0600

    refactor TestStudentEntity

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/TestStudentEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/TestStudentEntity.java
@@ -1,21 +1,34 @@
 package kg.peaksoft.peaksoftlmsab4.model.entity;
 
 import kg.peaksoft.peaksoftlmsab4.model.enums.TestResult;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
-@Entity
-@Table(name = "test_results")
-@NoArgsConstructor
-@AllArgsConstructor
+@ToString
 @Getter
 @Setter
-@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "test_results")
 public class TestStudentEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
