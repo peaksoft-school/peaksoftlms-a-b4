41329e135f9667b1f8e9a4300cbe52d59dddfb15
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 15:23:20 +0600

    refactor QuestionEntity

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/QuestionEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/QuestionEntity.java
@@ -1,23 +1,32 @@
 package kg.peaksoft.peaksoftlmsab4.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import kg.peaksoft.peaksoftlmsab4.model.enums.QuestionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "questions")
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "questions")
 public class QuestionEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,12 +35,12 @@ public class QuestionEntity {
     @Enumerated(EnumType.STRING)
     private QuestionType questionType;
 
-    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<OptionEntity> options;
 
-    public void setOption(OptionEntity option){
-        if (options==null){
-            options=new ArrayList<>();
+    public void setOption(OptionEntity option) {
+        if (options == null) {
+            options = new ArrayList<>();
         }
         options.add(option);
     }
