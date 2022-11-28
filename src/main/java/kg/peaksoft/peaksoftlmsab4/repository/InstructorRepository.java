c95060ebb9a632c0de8dbb7a33ec8b0a28526dfa
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 17:22:57 +0600

    refactor InstructorRepository

--- src/main/java/kg/peaksoft/peaksoftlmsab4/repository/InstructorRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/repository/InstructorRepository.java
@@ -1,16 +1,14 @@
 package kg.peaksoft.peaksoftlmsab4.repository;
 
 import kg.peaksoft.peaksoftlmsab4.model.entity.InstructorEntity;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface InstructorRepository extends JpaRepository<InstructorEntity,Long> {
+public interface InstructorRepository extends JpaRepository<InstructorEntity, Long> {
 
     @Query("select case when count(a)>0 then true else false end" +
             " from AuthInfo a where a.email =?1")
@@ -18,4 +16,5 @@ public interface InstructorRepository extends JpaRepository<InstructorEntity,Lon
 
     @Query("select i from InstructorEntity i where i.authInfo.email = ?1")
     Optional<InstructorEntity> findInstructorByEmail(String email);
+
 }
