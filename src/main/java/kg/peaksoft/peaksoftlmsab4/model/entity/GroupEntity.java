07c5be88573d259ad33b905d3e11350014ee81df
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 14:59:42 +0600

    refactor GroupEntity

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/GroupEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/GroupEntity.java
@@ -1,36 +1,48 @@
 package kg.peaksoft.peaksoftlmsab4.model.entity;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Builder
-@Entity
-@Table(name = "groups")
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "groups")
 public class GroupEntity {
+
     @Id
-    @SequenceGenerator(
-            name = "groups_sequence",
-            sequenceName = "groups_id_seq",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "groups_sequence"
-    )
+    @SequenceGenerator(name = "groups_gen", sequenceName = "groups_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "groups_gen")
     private Long id;
+
     private String groupName;
+
     private LocalDate dateOfStart;
+
     private String description;
+
     private String image;
 
     @JsonIgnore
