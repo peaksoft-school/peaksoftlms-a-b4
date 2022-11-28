cfac387b492f706ed0962f61a12f8867e6ef973b
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-28 15:47:00 +0600

    refactor VideoEntity

--- src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/VideoEntity.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/model/entity/VideoEntity.java
@@ -1,20 +1,35 @@
 package kg.peaksoft.peaksoftlmsab4.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-import static javax.persistence.CascadeType.*;
+import static javax.persistence.CascadeType.DETACH;
+import static javax.persistence.CascadeType.MERGE;
+import static javax.persistence.CascadeType.REFRESH;
 
 @Builder
-@Entity
-@Table(name = "videos")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "videos")
 public class VideoEntity {
+
     @Id
     @SequenceGenerator(
             name = "videos_sequence",
