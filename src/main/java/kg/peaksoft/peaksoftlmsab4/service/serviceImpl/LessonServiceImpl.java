5086eb27feff3a2d3a84835dfdffb718e8919407
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-29 14:27:18 +0600

    refactor LessonServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/LessonServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/LessonServiceImpl.java
@@ -3,23 +3,23 @@ package kg.peaksoft.peaksoftlmsab4.service.serviceImpl;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.request.LessonRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.response.LessonResponse;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.response.LessonResponseForGet;
-import kg.peaksoft.peaksoftlmsab4.validation.exception.BadRequestException;
-import kg.peaksoft.peaksoftlmsab4.validation.exception.NotFoundException;
 import kg.peaksoft.peaksoftlmsab4.model.entity.CourseEntity;
 import kg.peaksoft.peaksoftlmsab4.model.entity.LessonEntity;
 import kg.peaksoft.peaksoftlmsab4.model.mapper.LessonMapper;
 import kg.peaksoft.peaksoftlmsab4.repository.CourseRepository;
 import kg.peaksoft.peaksoftlmsab4.repository.LessonRepository;
 import kg.peaksoft.peaksoftlmsab4.service.LessonService;
+import kg.peaksoft.peaksoftlmsab4.validation.exception.BadRequestException;
+import kg.peaksoft.peaksoftlmsab4.validation.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Service
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class LessonServiceImpl implements LessonService {
 
     private final LessonMapper mapper;
@@ -28,13 +28,11 @@ public class LessonServiceImpl implements LessonService {
 
     @Override
     public LessonResponse create(LessonRequest request, Long courseId) {
-        CourseEntity courseEntity = courseRepository.findById(courseId)
-                .orElseThrow(() -> {
-                    log.error("Course with id = {} does not exists", courseId);
-                    throw new NotFoundException(
-                            String.format("Course with id = %s does not exists", courseId)
-                    );
-                });
+        CourseEntity courseEntity = courseRepository.findById(courseId).orElseThrow(() -> {
+            log.error("Course with id = {} does not exists", courseId);
+            throw new NotFoundException(
+                    String.format("Course with id = %s does not exists", courseId));
+        });
         LessonEntity lesson = mapper.mapToEntity(request);
         lesson.setCourseEntity(courseEntity);
         log.info(" Lesson with name : {} has successfully saved to database", lesson.getLessonName());
@@ -49,25 +47,20 @@ public class LessonServiceImpl implements LessonService {
 
     @Override
     public LessonResponseForGet getById(Long lessonId) {
-        LessonEntity lesson = lessonRepository.findById(lessonId).
-                orElseThrow(() -> {
-                    log.error("Lesson with id = {} does not exists", lessonId);
-                    throw new NotFoundException(
-                            String.format("Lesson with id = %s does not exists", lessonId)
-                    );
-                });
+        LessonEntity lesson = lessonRepository.findById(lessonId).orElseThrow(() -> {
+            log.error("Lesson with id = {} does not exists", lessonId);
+            throw new NotFoundException(
+                    String.format("Lesson with id = %s does not exists", lessonId));
+        });
         return mapper.mapToResponseForGetMethod(lesson);
     }
 
     @Override
     public LessonResponse update(Long lessonId, LessonRequest request) {
-        LessonEntity lesson = lessonRepository.findById(lessonId).
-                orElseThrow(() -> {
-                    log.error("Lesson with id = {} does not exists", lessonId);
-                    throw new NotFoundException(
-                            String.format("Lesson with id = %s does not exists", lessonId)
-                    );
-                });
+        LessonEntity lesson = lessonRepository.findById(lessonId).orElseThrow(() -> {
+            log.error("Lesson with id = {} does not exists", lessonId);
+            throw new NotFoundException(String.format("Lesson with id = %s does not exists", lessonId));
+        });
         mapper.update(lesson, request);
         return mapper.mapToResponse(lessonRepository.save(lesson));
     }
@@ -77,18 +70,14 @@ public class LessonServiceImpl implements LessonService {
         boolean existById = lessonRepository.existsById(id);
         if (!existById) {
             log.error("Lesson with id = {} does not exists, you can not delete it", id);
-            throw new BadRequestException(
-                    String.format("Lesson with id = %s does not exists, you can not delete it", id)
-            );
+            throw new BadRequestException(String.format("Lesson with id = %s does not exists, you can not delete it", id));
         }
-        LessonEntity lessonEntity=lessonRepository.findById(id).
-                orElseThrow(() -> {
-                    log.error("Lesson with id = {} does not exists", id);
-                    throw new NotFoundException(
-                            String.format("Lesson with id = %s does not exists", id)
-                    );
-                });
+        LessonEntity lessonEntity = lessonRepository.findById(id).orElseThrow(() -> {
+            log.error("Lesson with id = {} does not exists", id);
+            throw new NotFoundException(String.format("Lesson with id = %s does not exists", id));
+        });
         lessonRepository.deleteById(id);
-       return mapper.mapToResponse(lessonEntity);
+        return mapper.mapToResponse(lessonEntity);
     }
+
 }
