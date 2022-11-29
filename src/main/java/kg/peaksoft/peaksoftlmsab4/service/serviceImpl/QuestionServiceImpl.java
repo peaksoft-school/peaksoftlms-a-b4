d35460fb7b608b8164c9ac976d873d1dcc75956c
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-29 14:37:08 +0600

    refactor QuestionServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/QuestionServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/QuestionServiceImpl.java
@@ -2,30 +2,24 @@ package kg.peaksoft.peaksoftlmsab4.service.serviceImpl;
 
 import kg.peaksoft.peaksoftlmsab4.controller.payload.request.QuestionRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.response.QuestionResponse;
-import kg.peaksoft.peaksoftlmsab4.validation.exception.NotFoundException;
 import kg.peaksoft.peaksoftlmsab4.model.entity.QuestionEntity;
-import kg.peaksoft.peaksoftlmsab4.model.entity.TestEntity;
 import kg.peaksoft.peaksoftlmsab4.model.mapper.QuestionMapper;
 import kg.peaksoft.peaksoftlmsab4.repository.QuestionRepository;
-import kg.peaksoft.peaksoftlmsab4.repository.TestRepository;
-import kg.peaksoft.peaksoftlmsab4.repository.TestStudentRepository;
 import kg.peaksoft.peaksoftlmsab4.service.QuestionService;
+import kg.peaksoft.peaksoftlmsab4.validation.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class QuestionServiceImpl implements QuestionService {
-
 
     private final QuestionRepository questionRepository;
     private final QuestionMapper questionMapper;
-    private final TestRepository testRepository;
-    private final TestStudentRepository testStudentRepository;
 
     @Override
     public QuestionResponse create(Long id, QuestionRequest request) {
@@ -36,9 +30,7 @@ public class QuestionServiceImpl implements QuestionService {
     public QuestionResponse update(Long id, QuestionRequest request) {
         QuestionEntity question = questionRepository.findById(id).orElseThrow(() -> {
             log.error("Question with id = {} does not exists", id);
-            throw new NotFoundException(
-                    String.format("Question with id = %s does not exists", id)
-            );
+            throw new NotFoundException(String.format("Question with id = %s does not exists", id));
         });
         questionMapper.update(question, request);
         return questionMapper.viewQuestion(questionRepository.save(question));
@@ -48,9 +40,7 @@ public class QuestionServiceImpl implements QuestionService {
     public QuestionResponse findById(Long id) {
         return questionMapper.viewQuestion(questionRepository.findById(id).orElseThrow(() -> {
             log.error("Question with id = {} does not exists", id);
-            throw new NotFoundException(
-                    String.format("Question with id = %s does not exists", id)
-            );
+            throw new NotFoundException(String.format("Question with id = %s does not exists", id));
         }));
     }
 
@@ -68,25 +58,15 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public QuestionResponse findByQuestionToTest(Long testId, Long questionId) {
-        TestEntity test = testRepository.findById(testId)
-                .orElseThrow(() -> {
-                    log.error("Test with id = {} does not exists", testId);
-                    throw new NotFoundException(
-                            String.format("Test with id = %s does not exists", testId)
-                    );
-                });
         QuestionEntity question = getByIdMethod(questionId);
-
         return questionMapper.viewQuestion(questionRepository.save(question));
     }
 
     private QuestionEntity getByIdMethod(Long questionId) {
-        return questionRepository.findById(questionId).
-                orElseThrow(() -> {
-                    log.error("Question with id = {} does not exists", questionId);
-                    throw new NotFoundException(
-                            String.format("Question with id = %s does not exists", questionId)
-                    );
-                });
+        return questionRepository.findById(questionId).orElseThrow(() -> {
+            log.error("Question with id = {} does not exists", questionId);
+            throw new NotFoundException(String.format("Question with id = %s does not exists", questionId));
+        });
     }
+
 }
