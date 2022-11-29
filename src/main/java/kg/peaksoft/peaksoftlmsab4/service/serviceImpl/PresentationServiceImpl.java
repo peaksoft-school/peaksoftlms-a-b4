c1275bfd02bf3f32e7906923570838ae206e7912
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-29 14:34:33 +0600

    refactor PresentationServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/PresentationServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/PresentationServiceImpl.java
@@ -2,23 +2,23 @@ package kg.peaksoft.peaksoftlmsab4.service.serviceImpl;
 
 import kg.peaksoft.peaksoftlmsab4.controller.payload.request.PresentationRequest;
 import kg.peaksoft.peaksoftlmsab4.controller.payload.response.PresentationResponse;
-import kg.peaksoft.peaksoftlmsab4.validation.exception.BadRequestException;
-import kg.peaksoft.peaksoftlmsab4.validation.exception.NotFoundException;
 import kg.peaksoft.peaksoftlmsab4.model.entity.LessonEntity;
 import kg.peaksoft.peaksoftlmsab4.model.entity.PresentationEntity;
 import kg.peaksoft.peaksoftlmsab4.model.mapper.PresentationMapper;
 import kg.peaksoft.peaksoftlmsab4.repository.LessonRepository;
 import kg.peaksoft.peaksoftlmsab4.repository.PresentationRepository;
 import kg.peaksoft.peaksoftlmsab4.service.PresentationService;
+import kg.peaksoft.peaksoftlmsab4.validation.exception.BadRequestException;
+import kg.peaksoft.peaksoftlmsab4.validation.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class PresentationServiceImpl implements PresentationService {
 
     private final PresentationRepository presentationRepository;
@@ -38,6 +38,7 @@ public class PresentationServiceImpl implements PresentationService {
             throw new BadRequestException("In this lesson presentation already exists");
         }
     }
+
     @Override
     public List<PresentationResponse> getAll() {
         log.info("fount {} presentations", presentationRepository.findAll().size());
@@ -46,31 +47,29 @@ public class PresentationServiceImpl implements PresentationService {
 
     @Override
     public PresentationResponse getById(Long presentationId) {
-        PresentationEntity presentationEntity = presentationRepository.findById(presentationId)
-                .orElseThrow(() -> {
-                    throw new NotFoundException(String.format("presentation with id = %s does not exists", presentationId));
-                });
-        return mapper.mapToResponse(presentationEntity);
+        PresentationEntity presentation = presentationRepository.findById(presentationId).orElseThrow(() -> {
+            throw new NotFoundException(String.format("presentation with id = %s does not exists", presentationId));
+        });
+        return mapper.mapToResponse(presentation);
     }
 
     @Override
     public PresentationResponse update(Long presentationId, PresentationRequest request) {
-        PresentationEntity presentationEntity = presentationRepository.findById(presentationId)
-                .orElseThrow(() -> {
-                    throw new NotFoundException(String.format("Presentation with id = %s does not exists", presentationId));
-                });
-        mapper.update(presentationEntity, request);
-        presentationRepository.save(presentationEntity);
-        return mapper.mapToResponse(presentationEntity);
+        PresentationEntity presentation = presentationRepository.findById(presentationId).orElseThrow(() -> {
+            throw new NotFoundException(String.format("Presentation with id = %s does not exists", presentationId));
+        });
+        mapper.update(presentation, request);
+        presentationRepository.save(presentation);
+        return mapper.mapToResponse(presentation);
     }
 
     @Override
     public PresentationResponse deleteById(Long presentationId) {
-        PresentationEntity presentationEntity = presentationRepository.findById(presentationId)
-                .orElseThrow(() -> {
-                    throw new NotFoundException(String.format("presentation with id = %s does not exists", presentationId));
-                });
+        PresentationEntity presentation = presentationRepository.findById(presentationId).orElseThrow(() -> {
+            throw new NotFoundException(String.format("presentation with id = %s does not exists", presentationId));
+        });
         presentationRepository.deleteById(presentationId);
-        return mapper.mapToResponse(presentationEntity);
+        return mapper.mapToResponse(presentation);
     }
+
 }
