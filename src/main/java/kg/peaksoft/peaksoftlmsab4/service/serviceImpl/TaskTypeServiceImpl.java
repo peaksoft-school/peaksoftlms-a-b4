b2820655af3498853ddfc821112c71d0beb5f38a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-29 14:43:28 +0600

    refactor TaskTypeServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/TaskTypeServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsab4/service/serviceImpl/TaskTypeServiceImpl.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class TaskTypeServiceImpl implements TaskTypeService {
+
     private final TaskTypeRepository taskTypeRepository;
+
     @Override
     public String delete(Long id) {
         taskTypeRepository.deleteById(id);
         return "Task deleted";
     }
+
 }
